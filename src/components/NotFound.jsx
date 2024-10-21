78424f48aa5839c631ef8d4244531e0af46471fa
Avi Varma <HarshvardhanV98@gmail.com>
2024-10-21 14:46:39 -0600

    404.

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,7 +6,9 @@ const NotFound = () => {
   return (
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you're looking for doesn't exist.</p>
+      <p>Sorry, I couldn't find the page you're looking for.</p>
+      <img src="https://media.giphy.com/media/3o7TKUM3IgJBX2as9O/giphy.gif" alt="Page Not Found" />
+      <br />
       <Link to="/">Go back to Home</Link>
     </div>
   );
