4430420b50c391a5d026b14b5fc62aa20c43c3ef
Avi Varma <HarshvardhanV98@gmail.com>
2024-10-20 20:49:30 -0600

    Modified a lot of UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,20 +32,18 @@ const Contact = () => {
           </div>
         </div>
 
-        {/* Contact Form */}
-        <div className="contact-form">
-          <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Your name.." required />
-
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Your email.." required />
-
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" placeholder="Write something.." required></textarea>
-
-            <button type="submit" className="contact-submit-button">Send Message</button>
-          </form>
+        {/* Google Maps Embed */}
+        <div className="map-container">
+          <iframe
+            title="Google Maps Calgary"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100345.54493435022!2d-114.16927329526596!3d51.02784775426898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5371706b86f257b7%3A0xc0f38fe917e1d0a9!2sCalgary%2C%20AB%2C%20Canada!5e0!3m2!1sen!2sus!4v1697988485792!5m2!1sen!2sus"
+            width="100%"
+            height="450px"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </section>
